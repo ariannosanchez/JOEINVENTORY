c444d7a19928731e46adb0b5846be7ffbbdb3e90
arisama <ariannosanchezmix@outlook.com>
2022-12-04 21:18:39 -0500

    Diseño e implementacion del activity agregar prodcutos

--- app/src/main/java/pe/idat/joeinventory/view/fragmentos/F_Inventario.java
+++ app/src/main/java/pe/idat/joeinventory/view/fragmentos/F_Inventario.java
@@ -1,5 +1,6 @@
 package pe.idat.joeinventory.view.fragmentos;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import pe.idat.joeinventory.R;
+import pe.idat.joeinventory.view.AgregarProducto;
 
 
 public class F_Inventario extends Fragment {
+
+    FloatingActionButton FAB_AgregarProducto;
 
 //    @Override
 //    public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,15 @@ public class F_Inventario extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_f__inventario, container, false);
+
+        FAB_AgregarProducto = view.findViewById(R.id.FAB_AgregarProducto);
+        FAB_AgregarProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AgregarProducto.class));
+            }
+        });
+
         return view;
     }
 }
