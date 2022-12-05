e32e43f9f24e8a12fe20656a3a6d88ad4c804d06
arisama <ariannosanchezmix@outlook.com>
2022-12-04 20:44:40 -0500

    Agregando el floating action button

---
+++ app/src/main/java/pe/idat/joeinventory/view/AgregarProducto.java
@@ -0,0 +1,16 @@
+package pe.idat.joeinventory.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.idat.joeinventory.R;
+
+public class AgregarProducto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_producto);
+    }
+}
